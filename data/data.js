5f9365a2d357de353e432db9ef49e66c04a61fc3
Thiago Costa <thiagomoreiracosta@gmail.com>
2025-08-20 18:32:36 -0300

    me2car

--- data/data.js
+++ data/data.js
@@ -35,4 +35,16 @@ export const cars = [
     description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta animi quis perspiciatis molestiae ex quaerat impedit non consequatur accusamus et ducimus a est, repudiandae error itaque? Placeat modi explicabo mollitia.",
     image:"./assets/6.png",
   },
+  {
+    title:"Maserati",
+    subtitle:"MC20 Conversível",
+    description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta animi quis perspiciatis molestiae ex quaerat impedit non consequatur accusamus et ducimus a est, repudiandae error itaque? Placeat modi explicabo mollitia.",
+    image:"./assets/201-maserati-finance.png",
+  },
+  {
+    title:"DeLorean",
+    subtitle:"Time Machine Featured in Back to the Future",
+    description:"1981 DeLorean Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta animi quis perspiciatis molestiae ex quaerat impedit non consequatur accusamus et ducimus a est, repudiandae error itaque? Placeat modi explicabo mollitia.",
+    image:"./assets/1981-delorean-back-to-the-future.png",
+  },
 ]
